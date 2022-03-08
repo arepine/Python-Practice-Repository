7890e863a4aa7470804701b59f59e101e0e32c9e
arepine <repinea@yahoo.com>
2022-03-07 21:22:32 -0600

    Complete Programs

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,18 @@
+# # This program creates a madlibs generator
+# # @author: Austin Repine
+
+# youtuber = "Kylie Ying" #some string variable
+
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
