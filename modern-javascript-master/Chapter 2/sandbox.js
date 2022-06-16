735fb104df81edc809eb817b5c616d6336d8c50f
repine-austin <repine_austin@epsilon-inc.dev>
2022-06-16 13:07:36 -0500

    {add integration} terraform course files
    Added Udemy Terraform course files to my Github repository.

---
+++ modern-javascript-master/Chapter 2/sandbox.js
@@ -0,0 +1,13 @@
+let age = 25;
+let year = 2019;
+
+// console.log(age, year);
+// // comments yay
+// age = 30;
+// console.log(age);
+
+// const points = 100;
+// console.log(points);
+
+// var score = 75;
+// console.log(score);
